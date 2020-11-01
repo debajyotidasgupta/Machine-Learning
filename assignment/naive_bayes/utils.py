d3844c8f839240bb99b6db3717817cb2e1833bdd
Siba Smarak Panigrahi <sibasmarak.p@gmail.com>
2020-11-01 10:19:29 +0530

    updated utils.py

--- assignment/naive_bayes/utils.py
+++ assignment/naive_bayes/utils.py
@@ -1,50 +1,141 @@
-from csv import reader
-from random import seed, randrange, shuffle
+
+"""
+This python file contains the utility functions used in the assignment
+"""
+
+# Authors: Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
+#          Siba Smarak Panigrahi <sibasmarak.p@gmail.com>
+
+# import necessary modules 
+from csv import reader                        
 from math import sqrt, exp, pi
 from collections import Counter
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from random import seed, randrange, shuffle
 from sklearn.preprocessing import LabelEncoder
 
-def train_test_split(dataset, train_size=0.8): #utility
-  shuffle(dataset)
-  X_train = dataset[:int(0.8*len(dataset))]
-  X_test = dataset[int(0.8*len(dataset)):]
-  return X_train, X_test
+def train_test_split(dataset, train_size=0.8):
+    '''
+    This function splits the data into two sets - train and test
+    Parameters:
+    -----------
+    dataset: dataset to be split
+    train_size: the ratio of training data to original data
 
-# Split a dataset into k folds ===utility
+    Returns:
+    --------
+    X_train: training set
+    X_test: test set
+    '''
+
+    shuffle(dataset)
+    X_train = dataset[:int(0.8*len(dataset))]
+    X_test = dataset[int(0.8*len(dataset)):]
+    return X_train, X_test
+
 def cross_validation_split(dataset, n_folds):
-  dataset_split = list()
-  l = len(dataset) // n_folds
-  for i in range(n_folds):
-    dataset_split.append(dataset[i*l:(i+1)*l])
-  return dataset_split
+    '''
+    This function splits the input data and returns the indices of the validation set for each iteration of cross-validation
+    Parameters:
+    -----------
+    dataset: input data
+    n_folds: number of times cross-validation is to be done (for a 5-fold cross validation, n_folds = 5)
+
+    Returns:
+    --------
+    dataset_split: a n_fold length list of list of indices
+                   each entry of this list is a set of indices to be used as validation set in the n_fols cross validation 
+    '''
+
+    dataset_split = list()
+    l = len(dataset) // n_folds
+    for i in range(n_folds):
+        dataset_split.append(dataset[i*l:(i+1)*l])
+    return dataset_split
 
 def accuracy_metric(actual, predicted):
-	correct = 0
+    '''
+    This function evaluates the accuracy of predicted values with respect the actual values
+    Accuracy is defined as the number of correct predictions divided by total length of predictions
+    Parameters:
+    -----------
+    actual: list of actual values
+    predicted: list of predicted values
+
+    Returns:
+    --------
+    result: accuracy in %
+    '''
+
+    correct = 0
 	for i in range(len(actual)):
 		if actual[i] == predicted[i]:
 			correct += 1
-	return correct / float(len(actual)) * 100.0
+    result = correct / float(len(actual)) * 100.0
+	return result
 
-
-# Calculate the mean of a list of numbers ==utility
 def mean(numbers):
-	return sum(numbers)/float(len(numbers))
+    '''
+    This function returns the mean of the input numbers
+    Parameters:
+    -----------
+    numbers: list of numbers whose mean is to be found
 
-# Calculate the standard deviation of a list of numbers ==utility
+    Returns:
+    --------
+    mu: mean of the input nunmbers list
+    '''
+
+    mu = sum(numbers)/float(len(numbers))
+	return mu
+
 def stdev(numbers):
+    '''
+    This function returns the standard deviation of the input numbers
+    Parameters:
+    -----------
+    numbers: list of numbers whose mean is to be found
+
+    Returns:
+    --------
+    sigma: standard deviation of the input nunmbers list
+    '''
+
 	avg = mean(numbers)
 	variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
-	return sqrt(variance)
+    sigma = sqrt(variance)
+	return sigma
 
-# Calculate the mean, stdev and count for each column in a dataset  ===utility
 def summarize_dataset(dataset):
-  summaries = [(mean(column), stdev(column), len(column), mean(column)+3*stdev(column)) for column in zip(*dataset)]
-  del(summaries[-1])
-  return summaries
+    '''
+    This functions returns the mean, standard deviation, number of entries, threshold value for outlier for each column of the dataset (characteristic values)
+    Parameters:
+    -----------
+    dataset: input data for which the above mentioned characteristic values are to be evaluated for each column
 
+    Returns:
+    --------
+    summaries: list of list of characteristic values of columns of dataset
+    '''
 
-# Calculate the Gaussian probability distribution function for x ===utility
+    summaries = [(mean(column), stdev(column), len(column), mean(column)+3*stdev(column)) for column in zip(*dataset)]
+    del(summaries[-1])
+    return summaries
+
 def calculate_probability(x, mean, stdev):
+    '''
+    This function returns the probability density of the data point on a Gaussian with given input mean ad standard deviation
+    Parameters:
+    -----------
+    x: data point whose probability density is to be evaluated
+    mean: mean of the Gaussian
+    stdev: standard deviation of the Gaussian
+
+    Returns:
+    --------
+    prob: probability density of the data point x on the Gaussian with mu = mean and sigma = stdev
+    '''
+
 	exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
 	return (1 / (sqrt(2 * pi) * stdev)) * exponent
- 
